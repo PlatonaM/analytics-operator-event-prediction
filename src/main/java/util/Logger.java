4285ac940ec81d35d3915c5ee98b05123bb0d48d
y-du <42994541+y-du@users.noreply.github.com>
2021-03-16 16:12:50 +0100

    rename level and reduce available levels

--- src/main/java/util/Logger.java
+++ src/main/java/util/Logger.java
@@ -11,13 +11,10 @@ public class Logger {
     private final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Logger.class.getName());
 
     Map<String, Level> levelMap = Stream.of(new Object[][] {
-            { "severe", Level.SEVERE },
-            { "info", Level.INFO },
+            { "error", Level.SEVERE },
             { "warning", Level.WARNING },
-            { "config", Level.CONFIG },
-            { "fine", Level.FINE },
-            { "finer", Level.FINER },
-            { "finest", Level.FINEST },
+            { "info", Level.INFO },
+            { "debug", Level.FINEST },
     }).collect(Collectors.toMap(data -> (String) data[0], data -> (Level) data[1]));
 
     public Logger(String level) {
