94c9e597aabe034e56383a779680df4b82280c8e
y-du <42994541+y-du@users.noreply.github.com>
2021-03-16 10:45:39 +0100

    change type

--- src/main/java/models/JobData.java
+++ src/main/java/models/JobData.java
@@ -1,13 +1,14 @@
 package models;
 
 import java.util.List;
+import java.util.Map;
 
 public class JobData {
     public String time_field;
     public boolean sorted_data;
     public List<ModelData> models;
     public String status;
-    public String result;
+    public Map<String, List<Map<String, Number>>> result;
     public String reason;
 
     public JobData(String time_field, boolean sorted_data, List<ModelData> models) {
